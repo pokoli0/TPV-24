d56e70417411163845b075a88d7686d24c30adaa
pokoli <psierr01@ucm.es>
2024-10-10 13:55:35 +0200

    date arreglao

--- Lab1O/Lab1O/ListaPrestamos.h
+++ Lab1O/Lab1O/ListaPrestamos.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Prestamo.h"
 #include "Catalogo.h"
+#include "Date.hpp"
+#include <fstream>
+#include <iostream>
+
 class ListaPrestamos
 {
 private:
@@ -11,6 +15,7 @@ public:
 	ListaPrestamos(){};
 	ListaPrestamos(istream&, const Catalogo&);
 	~ListaPrestamos();
+	bool leerPrestamos(string fichero);
 	void ordena();
 	void insertaPrestamo(const Prestamo&);
 	friend void operator<<(ostream&, const ListaPrestamos&);
