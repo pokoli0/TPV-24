69cc61b433fc23f3b815d24d6bfda98c3ef9c381
pokoli <psierr01@ucm.es>
2024-11-16 19:44:29 +0100

    Update Block.cpp

--- PRACTICA 1/plantillaSDL/HolaSDL/Block.cpp
+++ PRACTICA 1/plantillaSDL/HolaSDL/Block.cpp
@@ -112,13 +112,11 @@ Collision Block::hit(const SDL_Rect& rect, bool fromPlayer)
 			}
 			tipoBloque = VACIO;
 		}
-		else if (tipoBloque == LADRILLO) 
+		else if (tipoBloque == LADRILLO && game->getMarioState(1)) 
 		{
 			isAlive = false; // el bloque se destruye
 		}
 	}
-
-
 
 	return col;
 }
