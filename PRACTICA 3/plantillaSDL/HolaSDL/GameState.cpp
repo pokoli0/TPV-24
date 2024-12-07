ac63842fca0ecc09539e4980213eb65916587ad9
[MarcosPRC] <marcopan@ucm.es>
2024-12-07 18:25:50 +0100

    aiaiaia poli que me echa la bronca y es q no estaba hecho el gamestate

--- PRACTICA 3/plantillaSDL/HolaSDL/GameState.cpp
+++ PRACTICA 3/plantillaSDL/HolaSDL/GameState.cpp
@@ -1,1 +1,19 @@
 #include "GameState.h"
+
+
+void GameState::handleEvent(const SDL_Event& event)
+{
+	for (auto handler : eventHandlers) {
+		handler->handleEvent(event);
+	}
+}
+
+void GameState::addEventListener(EventHandler* l)
+{
+	eventHandlers.push_back(l);
+}
+
+void GameState::addObject(GameObject* o)
+{
+	stateList.push_back(o);
+}
